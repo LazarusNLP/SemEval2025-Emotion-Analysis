e4c793a6d867d5bce841a161d6c7eff47cc274e4
w11wo <wilsonwong961@gmail.com>
2024-10-18 10:55:48 +1100

    Best NusaBERT Config

--- run_multilabel_classification.sh
+++ run_multilabel_classification.sh
@@ -1,9 +1,10 @@
 python src/run_multilabel_classification.py \
     --model_checkpoint LazarusNLP/NusaBERT-large \
-    --num_train_epochs 50 \
+    --num_train_epochs 100 \
     --optim adamw_torch_fused \
-    --learning_rate 2e-5 \
+    --learning_rate 1e-5 \
     --weight_decay 0.01 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 32 \
+    --early_stopping_patience 10 \
     --bf16
