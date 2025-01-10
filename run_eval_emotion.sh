114c248845b87eaa53cccd00974e19e680375235
w11wo <wilsonwong961@gmail.com>
2025-01-10 15:40:09 +1100

    Updated eval code to new dev dataset formats

--- run_eval_emotion.sh
+++ run_eval_emotion.sh
@@ -1,3 +1,14 @@
 accelerate launch src/eval_emotion.py \
-    --model_checkpoint "models/gemma2-9b-cpt-sea-lionv3-base-SemEval-sun" \
+    --test_file public_data_dev/track_a/dev/sun.csv \
+    --model_checkpoint "models/gemma2-9b-cpt-sahabatai-v1-base-SemEval-sun" \
+    --apply_liger_kernel_to_gemma2
+
+accelerate launch src/eval_emotion.py \
+    --test_file public_data_dev/track_c/dev/ind.csv \
+    --model_checkpoint "models/gemma2-9b-cpt-sahabatai-v1-base-SemEval-sun" \
+    --apply_liger_kernel_to_gemma2
+
+accelerate launch src/eval_emotion.py \
+    --test_file public_data_dev/track_c/dev/jav.csv \
+    --model_checkpoint "models/gemma2-9b-cpt-sahabatai-v1-base-SemEval-sun" \
     --apply_liger_kernel_to_gemma2
